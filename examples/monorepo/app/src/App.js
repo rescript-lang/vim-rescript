06b6386b53f195161cbfcb2e75df14ca0f2e8018
Dan Wetherald <dan.wetherald@gmail.com>
2020-12-17 18:21:52 -0600

    add app

---
+++ examples/monorepo/app/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { make as MyComponent } from "./components/MyComponent.bs";
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ examples/monorepo/app/src/components/MyComponent.res
@@ -0,0 +1,4 @@
+@react.component
+let make = () => {
+  <div> {"My Rescript Component"->React.string} <Awesomeness.Awesome /> </div>
+}
